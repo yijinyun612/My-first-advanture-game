2aa09ef7e6ae1263019eb858432171ae788c4dc3
一斤云 <87793903+yijinyun612@users.noreply.github.com>
2025-12-22 16:46:50 +0800

    Implement TransitionLayer for scene transitions

---
+++ TransitionLayer.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class TransitionLayer : CanvasLayer
+{
+	private ColorRect _colorRect;
+
+	public override void _Ready()
+	{
+		_colorRect = GetNode<ColorRect>("ColorRect");
+		_colorRect.Modulate = new Color(0, 0, 0, 0);
+	}
+
+	public async void ChangeScene(string targetPath)
+	{
+		var tween = CreateTween();
+
+		// fade-in
+		tween.TweenProperty(_colorRect, "modulate", new Color(0, 0, 0, 1), 0.5f);
+		await ToSignal(tween, Tween.SignalName.Finished);
+
+		// change scene
+		GetTree().ChangeSceneToFile(targetPath);
+
+		// fade-out
+		var tween2 = CreateTween();
+		tween2.TweenProperty(_colorRect, "modulate", new Color(0, 0, 0, 0), 0.5f);
+	}
+}
